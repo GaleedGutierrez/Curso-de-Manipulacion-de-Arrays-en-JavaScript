1c4f03ab349747acc2a6ff2714107bc8323db668
Galeed Gutierrez <galeedeveloper@gmail.com>
2022-03-03 02:11:58 -0300

    Clase N°18: Sort.

---
+++ server/Sort/17-sort.js
@@ -0,0 +1,59 @@
+const months = ["March", "Jan", "Feb", "Dec"];
+months.sort();
+// console.log({months});
+
+const numbers = [1, 30, 4, 21, 100000];
+numbers.sort((a, b) => b - a);
+// console.log({numbers});
+
+const words = 
+[
+    "réservé",
+    "premier",
+    "communiqué",
+    "café",
+    "adieu",
+    "éclair",
+    "banana",
+];
+words.sort((a, b) => a.localeCompare(b));
+// console.log({words});
+
+const orders = 
+[
+    {
+        customerName: "Nicolas",
+        total: 600,
+        delivered: true,
+    },
+    {
+        customerName: "Zulema",
+        total: 120,
+        delivered: false,
+    },
+    {
+        customerName: "Santiago",
+        total: 1840,
+        delivered: true,
+    },
+    {
+        customerName: "Valentina",
+        total: 240,
+        delivered: true,
+    },
+];
+// orders.sort((a, b) => b['total'] - a['total']);
+// console.log({orders});
+
+const ordersV2 = orders
+    .map(
+        (item, index) => {
+            return {
+                ...item,
+                date: new Date(2022, index, 2)
+            };
+        }
+    )
+    .sort((a, b) => b.date - a.date);
+
+// console.log({ordersV2});
